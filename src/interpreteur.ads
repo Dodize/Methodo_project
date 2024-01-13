06a2b5a97429ee9c9af2171c6282b34592bb5006
LeilaMartinet <leila.mrtnt1@gmail.com>
2024-01-13 12:03:51 +0100

    correction

--- src/interpreteur.ads
+++ src/interpreteur.ads
@@ -1,8 +1,11 @@
+WITH Ada.Text_IO ;
+USE Ada.Text_IO ;
+
 package Interpreteur is
 
    -- Permet de choisir le mode d'utilisation : 0 pour normal et 1 pour debug
    function Menu return Integer with
-     Post => res = 0 or res = 1;
+     Post => Menu'Result = 0 or Menu'Result = 1;
 
    -- Lance l'interpreteur en lisant et excecutant le code
    procedure Executer(Fichier : in File_Type);
