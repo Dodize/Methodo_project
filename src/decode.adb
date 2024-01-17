121bc2d885b3e82cc40c173dba4dc960b86855ff
Elodie Gastellu <gastellue@bde.enseeiht.fr>
2024-01-17 17:49:20 +0100

    creation de utils. et deplacement de slice

--- src/decode.adb
+++ src/decode.adb
@@ -77,30 +77,6 @@ package body Decode is
       Null;
    end instru_null;
 
-
-   -- Vérifier si delete inclu la supp du delimiteur comme souhaité
-   -- permet de retirer le premier mot avant délimiteur de la ligne en le plaçant dans mot
-   -- @param Ligne : ligne dont on extrait le mot
-   -- @param Mot : où placer le mot extrait
-   -- @param Delimiteur: caractère signant la fin du mot
-   procedure slice_mot(Ligne : in out Unbounded_String; Mot : out Unbounded_String; Delimiteur : in Character) is
-      Pos : Natural;
-   begin
-      -- Trouver l'indice du délimiteur
-      Pos := Ada.Strings.Unbounded.Index(Ligne, Character'Image(Delimiteur));
-      -- Extraire la sous-chaîne jusqu'au délimiteur
-      if Pos /= 0 then
-         Mot := Unbounded_Slice(Ligne, 1, Pos);
-         -- Supprimer la partie extraite de la ligne
-         Delete(Ligne, 1, Pos);
-      else
-         -- Si le délimiteur n'est pas trouvé, copier la ligne entière dans le mot
-         Mot := Ligne;
-         -- Réinitialiser la ligne
-         Delete(Ligne, 1, Length(Ligne));
-      end if;
-   end slice_mot;
-
    -- Rempli une ligne du tableau en mettant en forme l'instruction null ou un commentaire
    -- @param Tab : tableau a remplir
    -- @param Index : ligne du tableau a remplir
---
+++ src/utils.ads
@@ -0,0 +1,16 @@
+with Ada.Text_IO ;                use Ada.Text_IO ;
+with Ada.Integer_Text_IO;         use Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded;       use Ada.Strings.Unbounded;
+
+-- Ce module d�finit des fonctions utiles dans plusieurs modules afin d'eviter le code redondant
+
+package utils is
+
+   -- permet de retirer le premier mot avant délimiteur de la ligne en le plaçant dans mot
+   -- @param Ligne : ligne dont on extrait le mot
+   -- @param Mot : a placer le mot extrait
+   -- @param Delimiteur: caractere signant la fin du mot
+   procedure slice_mot(Ligne : in out Unbounded_String; Mot : out Unbounded_String; Delimiteur : in Character);
+   
+
+end utils;
