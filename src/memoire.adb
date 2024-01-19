79906c906f0188d52d4892597cbc826e7cf3526c
Elodie Gastellu <gastellue@bde.enseeiht.fr>
2024-01-19 10:47:12 +0100

    ajout de recuperer_type (attention commentaire pour decla variable)

--- src/memoire.adb
+++ src/memoire.adb
@@ -8,56 +8,57 @@ package body Memoire is
       package Mem_Integer is new Case_Memoire (T_Elt => Integer);
       package Mem_String is new Case_Memoire (T_Elt => Unbounded_String);
 
-      Current_Mem_Integer     : Mem_Integer.T_Memoire;
-      New_Mem_Integer : Mem_Integer.T_Memoire;
+      -- Current_Mem_Integer     : Mem_Integer.T_Memoire;
+      -- New_Mem_Integer : Mem_Integer.T_Memoire;
       Current_Line    : Unbounded_String;
       Temporary_Line  : Unbounded_String;
       Splitted_Line   : Unbounded_String;
-      Fini            : Boolean;
-      Current_Type    : T_Type;
+      -- Fini            : Boolean;
+      -- Current_Type    : T_Type;
    begin
-      Fini := False;
-      Initialiser(Mem);
-      Current_Mem_Integer := Mem;
-      -- Tant qu'on n'a pas atteint la fin du fichier ni la ligne "Debut"
-      while not End_Of_File (Code) and not Fini loop
-         Current_Line := To_Unbounded_String (Get_Line (Code));
-         Current_Line := Translate (Current_Line, Ada.Strings.Maps.Constants.Lower_Case_Map);
-         -- Si pas commentaire
-         if Index (Current_Line, "--") = 0 then
-            -- Verifier qu'on n'a pas atteint la ligne "Debut"
-            if
-              (Index (Temporary_Line, "début") > 0 or
-               Index (Temporary_Line, "debut") > 0) and
-              Index (Temporary_Line, ":") = 0
-            then
-               Fini := True;
-            else
-               -- Split au niveau de ':'
-               slice_mot (Current_Line, Splitted_Line, ':');
-               -- Recuperation du type
-               if Index (Splitted_Line, "entier") > 0 then
-                  Current_Type := ENTIER;
-               end if;
-               slice_mot (Current_Line, Splitted_Line, ',');
-               while Length (Current_Line) > 0 loop
-                  if Current_Type = ENTIER then
-                     Mem_Integer.Initialiser(New_Mem_Integer);
-                     New_Mem_Integer.Cle        := Splitted_Line;
-                     New_Mem_Integer.TypeOfData := Mem_Integer.ENTIER;
-
-                     if Est_Vide (Mem) then
-                        Mem := New_Mem_Integer;
-                     else
-                        Current_Mem.Suivant := New_Mem_Integer;
-                     end if;
-                  end if;
-                  -- Split au niveau de ','
-                  slice_mot (Current_Line, Splitted_Line, ',');
-               end loop;
-            end if;
-         end if;
-      end loop;
+      null;
+      --  Fini := False;
+      --  Initialiser(Mem);
+      --  Current_Mem_Integer := Mem;
+      --  -- Tant qu'on n'a pas atteint la fin du fichier ni la ligne "Debut"
+      --  while not End_Of_File (Code) and not Fini loop
+      --     Current_Line := To_Unbounded_String (Get_Line (Code));
+      --     Current_Line := Translate (Current_Line, Ada.Strings.Maps.Constants.Lower_Case_Map);
+      --     -- Si pas commentaire
+      --     if Index (Current_Line, "--") = 0 then
+      --        -- Verifier qu'on n'a pas atteint la ligne "Debut"
+      --        if
+      --          (Index (Temporary_Line, "début") > 0 or
+      --           Index (Temporary_Line, "debut") > 0) and
+      --          Index (Temporary_Line, ":") = 0
+      --        then
+      --           Fini := True;
+      --        else
+      --           -- Split au niveau de ':'
+      --           slice_mot (Current_Line, Splitted_Line, ':');
+      --           -- Recuperation du type
+      --           if Index (Splitted_Line, "entier") > 0 then
+      --              Current_Type := ENTIER;
+      --           end if;
+      --           slice_mot (Current_Line, Splitted_Line, ',');
+      --           while Length (Current_Line) > 0 loop
+      --              if Current_Type = ENTIER then
+      --                 Mem_Integer.Initialiser(New_Mem_Integer);
+      --                 New_Mem_Integer.Cle        := Splitted_Line;
+      --                 New_Mem_Integer.TypeOfData := Mem_Integer.ENTIER;
+      --
+      --                 if Est_Vide (Mem) then
+      --                    Mem := New_Mem_Integer;
+      --                 else
+      --                    Current_Mem.Suivant := New_Mem_Integer;
+      --                 end if;
+      --              end if;
+      --              -- Split au niveau de ','
+      --              slice_mot (Current_Line, Splitted_Line, ',');
+      --           end loop;
+      --        end if;
+      --     end if;
+      --  end loop;
    end DeclarerVariables;
 
    -- Modifie la donnee d'une variable existante en memoire
@@ -84,10 +85,26 @@ package body Memoire is
    -- @param Mem : la memoire dans laquelle est stockee la variable
    -- @param Cle : le nom de la variable recherchee
    -- @return le type de la variable
-   function RecupererType (Mem : in T_Memoire; Cle : in Unbounded_String) return Unbounded_String
-   is
+   function RecupererType (Mem : in T_Memoire; Cle : in Unbounded_String) return Unbounded_String is
+      Pos_entier : P_Memoire_Entier.T_Case_Memoire;
+      Pos_string : P_Memoire_String.T_Case_Memoire;
    begin
-      return To_Unbounded_String("CHAINE");
+      Pos_entier := Mem.Entiers;
+      -- Recherche si la cle est un entier
+      while (Pos_entier /= null) or else (Cle /= Pos_entier.Cle) loop
+         Pos_entier := Pos_entier.Suivant;
+      end loop;
+      if Pos_entier /= null then
+         return To_Unbounded_String("Entier");
+      end if;
+      Pos_string := Mem.Chaines;
+      while (Pos_string /= null) or else (Cle /= Pos_string.Cle) loop
+         Pos_string := Pos_string.Suivant;
+      end loop;
+      if Pos_string /= null then
+         return To_Unbounded_String("Chaine");
+      end if;
+      return To_Unbounded_String("null");
    end RecupererType;
 
 end Memoire;
