1d1747e9a53e26cb012902633b4130b3159e04ff
Elodie Gastellu <gastellue@bde.enseeiht.fr>
2024-01-25 08:51:44 +0100

    maj tabulation, spec

--- src/case_memoire.adb
+++ src/case_memoire.adb
@@ -1,18 +1,18 @@
 package body Case_Memoire is
 
-   -- Initialise une case memoire. La case memoire est vide
-   -- @param Mem : la memoire a initialiser
+    -- Initialise une case memoire. La case memoire est vide
+    -- @param Mem : la case memoire a initialiser
     procedure Initialiser(Mem : out T_Case_Memoire) is
     begin
-      Mem := null;
-   end Initialiser;
+        Mem := null;
+    end Initialiser;
 
-   -- Indique si la memoire est vide
-   -- @param Mem : la memoire
-   -- @return si la memoire est vide ou non
-   function Est_Vide(Mem : in T_Case_Memoire) return Boolean is
-   begin
-      return Mem = null;
-   end Est_Vide;
+    -- Indique si une case de la memoire est vide
+    -- @param Mem : la case memoire à tester
+    -- @return Boolean indiquant si la memoire est vide ou non
+    function Est_Vide(Mem : in T_Case_Memoire) return Boolean is
+    begin
+        return Mem = null;
+    end Est_Vide;
 
 end Case_Memoire;
--- src/case_memoire.ads
+++ src/case_memoire.ads
@@ -1,30 +1,33 @@
+-- Ce module definit les caractéristiques d'une case mémoire
+-- Utilisation de la généricité pour le type de la case mémoire
+
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 generic
-   type T_Elt is private;
+    type T_Elt is private;
 
 package Case_Memoire is
 
-   -- Enumerable du type de la variable
-   type T_Type is (CHAINE, ENTIER, TAB);
+    -- Enumerable du type de la variable
+    type T_Type is (CHAINE, ENTIER, TAB);
 
-	type T_Var;
-	type T_Case_Memoire is access T_Var;
-	type T_Var is
-      record
-         Cle: Unbounded_String;
-         Data : T_Elt;
-         TypeOfData : T_Type;
-         Suivant : T_Case_Memoire;
-      end record;
+    type T_Var;
+    type T_Case_Memoire is access T_Var;
+    type T_Var is
+        record
+            Cle: Unbounded_String;
+            Data : T_Elt;
+            TypeOfData : T_Type;
+            Suivant : T_Case_Memoire;
+        end record;
 
-   -- Initialise une case memoire. La case memoire est vide
-   -- @param Mem : la memoire a initialiser
+    -- Initialise une case memoire. La case memoire est vide
+    -- @param Mem : la case memoire a initialiser
     procedure Initialiser(Mem : out T_Case_Memoire);
 
-   -- Indique si la memoire est vide
-   -- @param Mem : la memoire
-   -- @return si la memoire est vide ou non
-   function Est_Vide(Mem : in T_Case_Memoire) return Boolean;
+    -- Indique si une case de la memoire est vide
+    -- @param Mem : la case memoire à tester
+    -- @return Boolean indiquant si la memoire est vide ou non
+    function Est_Vide(Mem : in T_Case_Memoire) return Boolean;
 
 end Case_Memoire;
