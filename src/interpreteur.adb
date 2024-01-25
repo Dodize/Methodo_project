6e3ef624af577449c8151359f11adc27ce8315fd
Elodie Gastellu <gastellue@bde.enseeiht.fr>
2024-01-25 09:12:55 +0100

    maj spec, tab, ajout exception menu

--- src/interpreteur.adb
+++ src/interpreteur.adb
@@ -18,56 +18,62 @@ package body Interpreteur is
             Get(type_menu);
         end loop;
         return type_menu;
-   end Menu;
 
-   -- Calcule la capacite du tableau d'instructions en fonction du nombre de ligne effective du fichier du code intermediaire
-   -- (entre les mots "Début" ou "Debut" et "Fin")
-   -- @param : in le fichier d'instructions en code intermediaire
-   -- @return : la capacite
-   function CalculerCapacite(NomFichier : in String) return Integer is
-      Capacite : Integer;
-      Fichier : File_Type;
-   begin
-      Ouvrir_Fichier_Lecture(NomFichier, Fichier);
+    exception
+        when others =>
+            -- Gérer l'exception Constraint_Error
+            Ada.Text_IO.Put_Line("Erreur : Entrez 0 ou 1.");
+            -- Supprimer la ligne incorrecte de l'entrée
+            Skip_Line;
+            return Menu;
+    end Menu;
 
-      -- Parcourir le fichier jusqu'à "début"
-      parcourir_debut(Fichier);
+    -- Calcule la capacite du tableau d'instructions en fonction du nombre de ligne effective du fichier du code intermediaire
+    -- (entre les mots "Début" et "Fin")
+    -- @param : in le fichier d'instructions en code intermediaire
+    -- @return : la capacite
+    function CalculerCapacite(NomFichier : in String) return Integer is
+        Capacite : Integer;
+        Fichier : File_Type;
+    begin
+        Ouvrir_Fichier_Lecture(NomFichier, Fichier);
 
-      -- Calculer le nombre de lignes de la ligne début + 1 / jusqu'à la fin du programme (mot "Fin)
-      Capacite := 0;
-      while not (Get_Line(Fichier) = "Fin") loop
-         Capacite := Capacite + 1;
-      end loop;
-      Close(Fichier);
-      return Capacite;
+        -- Parcourir le fichier jusqu'à "Début"
+        parcourir_debut(Fichier);
 
-   end CalculerCapacite;
+        -- Calculer le nombre de lignes de la ligne début + 1 / jusqu'à la fin du programme (mot "Fin")
+        Capacite := 0;
+        while not (Get_Line(Fichier) = "Fin") loop
+            Capacite := Capacite + 1;
+        end loop;
+        Close(Fichier);
+        return Capacite;
+    end CalculerCapacite;
 
-    -- Lance l'interpreteur en lisant et excecutant le code
+    -- Lance l'interpreteur en lisant et excecutant le code intermédiaire
     procedure Executer(NomFichier : in String) is
         package P_Decode is new Decode(Capacite => CalculerCapacite(NomFichier));
         use P_Decode;
         Instructions : T_tab_instruc;
         Mem : T_Memoire;
-        cp : Integer;
+        CP : Integer;
         Capacite_tab : Integer;
-        mode : Integer;
+        Mode : Integer;
     begin
-        -- Declaration des variables en memoire
+        -- Declaration des variables en memoire, récuperation des instructions
         DeclarerVariables(Mem, NomFichier);
-        -- Recuperation des instructions
         remplir_tab_instruc(Instructions, NomFichier);
         Capacite_tab := get_nombre_instruc(Instructions);
         -- Initialisation du CP
-        init_CP(cp);
-        -- Demande le menu
+        init_CP(CP);
+        -- Demande le Mode : normal ou debugg
         mode := menu;
         -- parcour le tableau tant que l'on n'a pas termine le programme
-        while (cp <= Capacite_tab) loop
-            if mode = 1 then
-                afficher(Instructions, cp, mem);
+        while (CP <= Capacite_tab) loop
+            if Mode = 1 then
+                afficher(Instructions, CP, Mem);
             end if;
-            effectuer_instru(Instructions, cp, Mem);
+            effectuer_instru(Instructions, CP, Mem);
         end loop;
     end Executer;
 
--- src/interpreteur.ads
+++ src/interpreteur.ads
@@ -1,13 +1,14 @@
-
+-- Ce module permet d'appeler notre interpréteur grâce au nom du fichier contenant le code intermédiaire
+-- Ce dernier lancera l'exécution de ce code intermédiaire en mode normal ou débugg
 
 package Interpreteur is
 
-   -- Permet de choisir le mode d'utilisation : 0 pour normal et 1 pour debug
-   -- @return : 0 pour mode d'execution normal, 1 pour le mode debug
-   function Menu return Integer with
-     Post => Menu'Result = 0 or Menu'Result = 1;
+    -- Permet de choisir le mode d'utilisation : 0 pour normal et 1 pour debug
+    -- @return : 0 pour mode d'execution normal, 1 pour le mode debug
+    function Menu return Integer with
+            Post => Menu'Result = 0 or Menu'Result = 1;
 
-   -- Lance l'interpreteur en lisant et excecutant le code
-   procedure Executer(NomFichier : in String);
+    -- Lance l'interpreteur en lisant et excecutant le code intermédiaire
+    procedure Executer(NomFichier : in String);
 
 end Interpreteur;
